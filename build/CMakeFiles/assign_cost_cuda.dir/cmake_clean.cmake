ab5d3507d4c76f06b0d91879e95493b114e23f65
patrick <patrickzhn96@gmail.com>
2024-08-02 15:24:55 +0800

    fix memory leakage

---
+++ build/CMakeFiles/assign_cost_cuda.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/assign_cost_cuda.dir/assign_cost_cuda_generated_assign_cost_cuda.cu.o"
+  "assign_cost_cuda.pdb"
+  "assign_cost_cuda.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/assign_cost_cuda.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
